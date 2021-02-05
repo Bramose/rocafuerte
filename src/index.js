e270297f77b40fe50609caf0770a4df478482ac6
Sergio Bravo Montoya <bramose_123@hotmail.com>
2021-02-05 15:08:35 -0600

    Segundo avance de paguina web

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 // import { saludar } from './js/componentes.js';
 import './styles.css';
 
+const btnAboutUs = document.querySelector("#btnAboutUs");
+
+btnAboutUs.addEventListener("click", async(event) => {
+    let btn = event.target;
+    if (btn.classList.contains('active')) {
+        return;
+    } else {
+        await removeActiveBtnAbout();
+        btn.classList.add("active");
+        document.querySelector(`#${btn.dataset.id}`).classList.remove("d-none");
+    }
+});
+
+const removeActiveBtnAbout = async() => {
+    for (let btn of btnAboutUs.children) {
+        if (btn.classList.contains("active")) {
+            btn.classList.remove("active");
+            document.querySelector(`#${btn.dataset.id}`).classList.add("d-none");
+        }
+    }
+}
+
+
 
 
 // const nombre = 'Fernando';
